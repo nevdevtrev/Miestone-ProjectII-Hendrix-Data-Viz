f1854fa775a3a75d549a96e7e261d5601dee57bb
EC2 Default User <ec2-user@ip-172-31-82-160.ec2.internal>
2019-11-25 09:20:56 +0000

    Basic first chart test

---
+++ design/js/chart.js
@@ -0,0 +1,34 @@
+queue()
+    .defer(d3.csv, "assets/Hendrix.csv")
+    .await(createCharts);
+    
+
+    
+function createCharts(error, hendrixData) {
+   var ndx = crossfilter(hendrixData);
+
+  show_sfps_per_person();
+  
+    
+}
+
+
+function show_sfps_per_person(ndx){
+    var dim = ndx.dimension(dc.pluck('Created By'));
+    var group = dim.group().reduceSum(dc.pluck('SFP No.'));
+    
+    dc.barchart("#sfps_per_person_chart")
+    .width(800)
+    .height(800)
+    .dimension(dim)
+    .group(group)
+    .margins({top:10, right:50, bottom:30, left:50})
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal())
+    .elasticY(true)
+    .xAxisLabel("People")
+    .yAxis().ticks(6);
+    
+     dc.renderAll();
+}
